c8009598a008bf31753e50dabb433c4e7e241a12
LucasTruebisch <45147275+LucasTruebisch@users.noreply.github.com>
2019-06-08 19:43:28 +0200

    Anfang Level1

---
+++ RoboscapeAbilities/Assets/ButtonManagerScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManagerScript : MonoBehaviour
+{
+    public GameObject Gate1;
+
+    static bool stateButton1 = false;
+    static bool stateButton2 = false;
+    int gate1_moved = 0;
+
+    private void Update()
+    {
+        if(stateButton1 || stateButton2)
+        {
+            if(gate1_moved < 20)
+            {
+                Gate1.transform.Translate(0, 0.15f, 0);
+                gate1_moved++;
+            }
+        }
+        else
+        {
+            if(gate1_moved > 0)
+            {
+                Gate1.transform.Translate(0, -0.15f, 0);
+                gate1_moved--;
+            }
+        }
+    }
+
+    public static void ButtonChange(string button, bool state)
+    {
+        if (button == "Button1")
+            stateButton1 = state;
+        else if (button == "Button2")
+            stateButton2 = state;
+    }
+
+}
--- RoboscapeAbilities/Assets/Scripts/Buttons/All_FloorButton.cs
+++ RoboscapeAbilities/Assets/Scripts/Buttons/All_FloorButton.cs
@@ -1,53 +1,78 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class All_FloorButton : MonoBehaviour
 {
-
+    public GameObject Button;
     protected bool pressed = false;
-    protected SpriteRenderer spriteRend;
+    bool pressedOld = false;
 
+    protected SpriteRenderer spriteRend;
+    public BoxCollider2D RobotCollider;
+    public BoxCollider2D ProfCollider;
+
+    string name;
+
+    int translateButton = 0;
 
     void Start()
     {
         spriteRend = GetComponent<SpriteRenderer>();
+        name = Button.name;
+        Debug.Log(name);
+    }
+
+    private void Update()
+    {
+        if(pressed && translateButton < 15)
+        {
+            Button.transform.Translate(0, -0.015f, 0);
+            translateButton++;
+        }
+        else if (!pressed && translateButton > 0)
+        {
+            Button.transform.Translate(0, 0.015f, 0);
+            translateButton--;
+        }
+
+        if(pressedOld != pressed)
+        {
+            pressedOld = pressed;
+            ButtonManagerScript.ButtonChange(name, pressed);
+        }
     }
 
     void OnTriggerStay2D(Collider2D col1)
     {
+        if(col1 == RobotCollider || col1 == ProfCollider)
+        {
+            if (!pressed && Math.Abs(col1.transform.position.x - Button.transform.position.x) < 0.3)
+            {
+                spriteRend.color = Color.green;
+                pressed = true;
+                col1.gameObject.transform.Translate(0, 0.15f, 0);
+            }
+            if (pressed && Math.Abs(col1.transform.position.x - Button.transform.position.x) >= 0.3)
+            {
+                spriteRend.color = Color.white;
+                pressed = false;
+            }
+        }
 
-        
-            Debug.Log("TriggerStay: All");
-            spriteRend.color = Color.green;
-            pressed = true;
         
     }
 
     void OnTriggerExit2D(Collider2D col2)
     {
-
-            Debug.Log("TriggerExit: All");
+        if (col2 != RobotCollider || col2 == ProfCollider)
+        {
             spriteRend.color = Color.white;
             pressed = false;
-        
-
-    }
-
-    /*void OnCollisionEnter2D(Collision2D col)
-    {
-        if (!pressed)
-        {
-
-            Debug.Log("Collision with an entity");
-            spriteRend.color = Color.green;
-            pressed = true;
-
-            //add switch case for different actions on different buttons
-
         }
 
-    }*/
+    }
 
 
 }
