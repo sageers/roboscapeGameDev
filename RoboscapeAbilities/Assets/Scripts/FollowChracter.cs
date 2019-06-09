373b7f1bc64aeeaf2b0f9020f071cbaa4fe85d49
LucasTruebisch <45147275+LucasTruebisch@users.noreply.github.com>
2019-06-09 19:57:13 +0200

    Camera Transitions

--- RoboscapeAbilities/Assets/Scripts/FollowChracter.cs
+++ RoboscapeAbilities/Assets/Scripts/FollowChracter.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
 
 public class FollowChracter : MonoBehaviour
 {
@@ -14,91 +16,191 @@ public class FollowChracter : MonoBehaviour
     float y;
     float z;
 
+    int level = 0;
+
+    bool allowTransitionsX = true;
+    bool allowTransitionsY = true;
+
+    float level1_offsetY = 2;
+
+    bool freeCamera = true;
+    GameObject[] CameraTransitions;
+    GameObject[] CameraTargets;
+    GameObject[] ResetTransitions;
+    enum trans_modes {non, doing, done };
+    int[] translationMode;
+    int currentTransition = -1;
+
     void Start()
     {
         x = camera.transform.position.x;
         y = camera.transform.position.y;
         z = camera.transform.position.z;
+        if (SceneManager.GetActiveScene().name == "Level1")
+        {
+            level = 1;
+            allowTransitionsY = false;
+            y = (prof.transform.position.y + robo.transform.position.y) / 2 + level1_offsetY;
+        }
+            
+
+        CameraTransitions = GameObject.FindGameObjectsWithTag("CameraTransition");
+        CameraTargets = new GameObject[CameraTransitions.Length];
+        ResetTransitions = new GameObject[CameraTransitions.Length];
+        for (int i = 0; i < CameraTransitions.Length; i++)
+        {
+            char[] CameraTransitions_Char = CameraTransitions[i].name.ToCharArray();
+            CameraTargets[i] = GameObject.Find("CameraTarget" + CameraTransitions_Char[CameraTransitions_Char.Length-1]);
+            ResetTransitions[i] = GameObject.Find("ResetTransition" + CameraTransitions_Char[CameraTransitions_Char.Length - 1]);
+        }
+        translationMode = new int[CameraTransitions.Length];
+        for(int i = 0; i < translationMode.Length; i++)
+        {
+            translationMode[i] = (int)trans_modes.non;
+        }
+
     }
 
 
     void Update()
     {
 
-        if (robo.transform.position.x < camera.transform.position.x - cameraRangeX)
+
+
+        if (freeCamera)
         {
-            if (prof.transform.position.x < camera.transform.position.x - cameraRangeX)
+            for(int i = 0; i < CameraTransitions.Length; i++)
             {
-                if (robo.transform.position.x < prof.transform.position.x)
+                if(translationMode[i] == (int)trans_modes.non && Vector3.Distance(CameraTransitions[i].transform.position,(prof.transform.position + robo.transform.position)/2) < 0.25f)
+                {
+                    freeCamera = false;
+                    currentTransition = i;
+                    translationMode[i] = (int)trans_modes.doing;
+
+                    goto Transition;
+                }
+            }
+
+            for(int i = 0; i < ResetTransitions.Length; i++)
+            {
+                if (translationMode[i] == (int)trans_modes.done && Vector2.Distance(ResetTransitions[i].transform.position, (robo.transform.position + prof.transform.position) / 2) < 0.25f)
+                    translationMode[i] = (int)trans_modes.non;
+            }
+
+
+            if (robo.transform.position.x < camera.transform.position.x - cameraRangeX)
+            {
+                if (prof.transform.position.x < camera.transform.position.x - cameraRangeX)
+                {
+                    if (robo.transform.position.x < prof.transform.position.x)
+                        x = robo.transform.position.x + cameraRangeX;
+                    else
+                        x = prof.transform.position.x + cameraRangeX;
+                }
+                else if (prof.transform.position.x > camera.transform.position.x + cameraRangeX)
+                {
+                    x = (prof.transform.position.x + robo.transform.position.x) / 2;
+                }
+                else
                     x = robo.transform.position.x + cameraRangeX;
+            }
+            else if (robo.transform.position.x > camera.transform.position.x + cameraRangeX)
+            {
+                if (prof.transform.position.x > camera.transform.position.x + cameraRangeX)
+                {
+                    if (robo.transform.position.x > prof.transform.position.x)
+                        x = robo.transform.position.x - cameraRangeX;
+                    else
+                        x = prof.transform.position.x - cameraRangeX;
+                }
+                else if (prof.transform.position.x < camera.transform.position.x - cameraRangeX)
+                {
+                    x = (prof.transform.position.x + robo.transform.position.x) / 2;
+                }
                 else
-                    x = prof.transform.position.x + cameraRangeX;
-            }
-            else if (prof.transform.position.x > camera.transform.position.x + cameraRangeX)
-            {
-                x = (prof.transform.position.x + robo.transform.position.x) / 2;
-            }
-            else
-                x = robo.transform.position.x + cameraRangeX;
-        }
-        else if (robo.transform.position.x > camera.transform.position.x + cameraRangeX)
-        {
-            if (prof.transform.position.x > camera.transform.position.x + cameraRangeX)
-            {
-                if (robo.transform.position.x > prof.transform.position.x)
                     x = robo.transform.position.x - cameraRangeX;
-                else
-                    x = prof.transform.position.x - cameraRangeX;
             }
             else if (prof.transform.position.x < camera.transform.position.x - cameraRangeX)
-            {
-                x = (prof.transform.position.x + robo.transform.position.x) / 2;
-            }
-            else
-                x = robo.transform.position.x - cameraRangeX;
-        }
-        else if (prof.transform.position.x < camera.transform.position.x - cameraRangeX)
-            x = prof.transform.position.x + cameraRangeX;
-        else if (prof.transform.position.x > camera.transform.position.x + cameraRangeX)
-            x = prof.transform.position.x - cameraRangeX;
+                x = prof.transform.position.x + cameraRangeX;
+            else if (prof.transform.position.x > camera.transform.position.x + cameraRangeX)
+                x = prof.transform.position.x - cameraRangeX;
 
-        if (robo.transform.position.y < camera.transform.position.y - cameraRangeY)
-        {
-            if (prof.transform.position.y < camera.transform.position.y - cameraRangeY)
+            /*if (robo.transform.position.y < camera.transform.position.y - cameraRangeY)
             {
-                if (robo.transform.position.y < prof.transform.position.y)
+                if (prof.transform.position.y < camera.transform.position.y - cameraRangeY)
+                {
+                    if (robo.transform.position.y < prof.transform.position.y)
+                        y = robo.transform.position.y + cameraRangeY;
+                    else
+                        y = prof.transform.position.y + cameraRangeY;
+                }
+                else if (prof.transform.position.y > camera.transform.position.y + cameraRangeY)
+                {
+                    y = (prof.transform.position.y + robo.transform.position.y) / 2;
+                }
+                else
                     y = robo.transform.position.y + cameraRangeY;
+            }
+            else if (robo.transform.position.y > camera.transform.position.y + cameraRangeY)
+            {
+                if (prof.transform.position.y > camera.transform.position.y + cameraRangeY)
+                {
+                    if (robo.transform.position.y > prof.transform.position.y)
+                        y = robo.transform.position.y - cameraRangeY;
+                    else
+                        y = prof.transform.position.y - cameraRangeY;
+                }
+                else if (prof.transform.position.y < camera.transform.position.y - cameraRangeY)
+                {
+                    y = (prof.transform.position.y + robo.transform.position.y) / 2;
+                }
                 else
-                    y = prof.transform.position.y + cameraRangeY;
-            }
-            else if (prof.transform.position.y > camera.transform.position.y + cameraRangeY)
-            {
-                y = (prof.transform.position.y + robo.transform.position.y) / 2;
-            }
-            else
-                y = robo.transform.position.y + cameraRangeY;
-        }
-        else if (robo.transform.position.y > camera.transform.position.y + cameraRangeY)
-        {
-            if (prof.transform.position.y > camera.transform.position.y + cameraRangeY)
-            {
-                if (robo.transform.position.y > prof.transform.position.y)
                     y = robo.transform.position.y - cameraRangeY;
-                else
-                    y = prof.transform.position.y - cameraRangeY;
             }
             else if (prof.transform.position.y < camera.transform.position.y - cameraRangeY)
+                y = prof.transform.position.y + cameraRangeY;
+            else if (prof.transform.position.y > camera.transform.position.y + cameraRangeY)
+                y = prof.transform.position.y - cameraRangeY;*/
+
+        }
+
+    Transition:
+        if (!freeCamera) {
+            if (Vector2.Distance(camera.transform.position, CameraTargets[currentTransition].transform.position) > 0.2f)
             {
-                y = (prof.transform.position.y + robo.transform.position.y) / 2;
+                float x_ = camera.transform.position.x - CameraTargets[currentTransition].transform.position.x;
+                float y_ = camera.transform.position.y - CameraTargets[currentTransition].transform.position.y;
+                if (allowTransitionsX)
+                {
+                    if (x_ > 0.2f)
+                        x = camera.transform.position.x - 0.1f;
+                    else if (x < 0.2f)
+                        x = camera.transform.position.x + 0.1f;
+                }
+                if (allowTransitionsY)
+                {
+                    if (y_ > 0.2f)
+                        y = camera.transform.position.y - 0.1f;
+                    else if (y < 0.2f)
+                        y = camera.transform.position.y + 0.1f;
+                }
             }
             else
-                y = robo.transform.position.y - cameraRangeY;
+            {
+                freeCamera = true;
+                translationMode[currentTransition] = (int)trans_modes.done;
+                currentTransition = -1;
+                
+            }
         }
-        else if (prof.transform.position.y < camera.transform.position.y - cameraRangeY)
-            y = prof.transform.position.y + cameraRangeY;
-        else if (prof.transform.position.y > camera.transform.position.y + cameraRangeY)
-            y = prof.transform.position.y - cameraRangeY;
+        if(level == 1)
+        {
+            
+        }
 
+        else 
+            y = (prof.transform.position.y + robo.transform.position.y) / 2;
+        
         camera.transform.position = new Vector3(x,y,z);
 
     }
